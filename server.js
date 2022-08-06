cbe569b1e3b707b7a01ee02f989914e2a024378b
Sakshi Nayan <sakshi12princi@gmail.com>
2022-08-06 20:26:23 +0530

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const orderController = require("./routes/order")
 const register = require("./routes/register&signin")
 //console.log(productController)
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({extended: false}));
 
 app.listen(process.env.PORT || 3001,(err)=>{
